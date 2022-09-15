29a1a88e193e0fab4337d33c755f651421f8d2a1
Eduarda Calderan <eduardanicolodicalderan@gmail.com>
2022-09-14 21:58:51 -0300

    starting about me

--- src/components/AbouteMe/index.tsx
+++ src/components/AbouteMe/index.tsx
@@ -1,7 +1,51 @@
+import { Content } from "./style";
+
 export function AboutMe() {
   return (
     <>
-      <p>about me</p>
+      <Content>
+        <div className="sidebar">
+          <div className="icons">i</div>
+          <div className="menu">
+            <p>sidebar</p>
+          </div>
+        </div>
+        <p>
+          /**
+          <br />
+          * About me
+          <br />
+          * I have 1 year and 5 months of еxperience in web
+          <br />
+          * development lorem ipsum dolor sit amet,
+          <br />
+          * consectetur adipiscing elit, sed do eiusmod
+          <br />
+          * tempor incididunt ut labore et dolore
+          <br />
+          * magna aliqua. Ut enim ad minim veniam,
+          <br />
+          * quis nostrud exercitation ullamco laboris
+          <br />
+          * nisi ut aliquip ex ea commodo consequat.
+          <br />
+          * Duis aute irure dolor in reprehenderit in
+          <br />
+          * <br />
+          * Duis aute irure dolor in reprehenderit in
+          <br />
+          * voluptate velit esse cillum dolore eu fugiat
+          <br />
+          * nulla pariatur. Excepteur sint occaecat
+          <br />
+          * officia deserunt mollit anim id est laborum.
+          <br />
+          */
+        </p>
+        <div>
+          <p>sidebar</p>
+        </div>
+      </Content>
     </>
   );
 }
---
+++ src/components/AbouteMe/style.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  .sidebar {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .icons {
+    padding: 1rem;
+  }
+
+  .menu {
+    padding: 1rem 0;
+  }
+`;
